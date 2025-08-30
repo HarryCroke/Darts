b477515d0ad7fa1edd7029797ea551450f2251b6
Harry Croker <hj.croker@gmail.com>
2025-08-30 19:39:43 +0100

    Feat: Swipe to move box

--- DartsGame/Assets/Scripts/SwipeInput.cs
+++ DartsGame/Assets/Scripts/SwipeInput.cs
@@ -7,6 +7,8 @@ using UnityEngine.InputSystem;
 public class SwipeInput : MonoBehaviour
 {
     private TouchControls touchControls;
+    private Vector2 swipeStart, swipeEnd;
+    public Rigidbody rb;
 
     private void Awake()
     {
@@ -28,13 +30,27 @@ public class SwipeInput : MonoBehaviour
 
     private void TouchStarted(InputAction.CallbackContext context)
     {
-        print(touchControls.Touch.Position.ReadValue<Vector2>());
+        swipeStart = touchControls.Touch.Position.ReadValue<Vector2>();
     }
     
     private void TouchEnded(InputAction.CallbackContext context)
     {
-        print(touchControls.Touch.Position.ReadValue<Vector2>());
+        swipeEnd = touchControls.Touch.Position.ReadValue<Vector2>();
+        Vector2 swipeVector = swipeStart - swipeEnd;
+        float swipeMagnitude = swipeVector.magnitude;
+        Vector2 swipeDirection = swipeVector.normalized;
+        print(swipeStart + " + " + swipeEnd + " = " + swipeVector);
+        print(swipeDirection + "     " + swipeMagnitude);
+        
+        Launch(swipeDirection, swipeMagnitude / 100);
+        
     }
-    
-    
+
+    private void Launch(Vector2 direction, float force)
+    {
+        Vector3 forceVector = new Vector3(direction.x, -direction.y, 0) * force;
+        rb.AddForce(forceVector, ForceMode.Impulse);
+    }
+
+
 }
