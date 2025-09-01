e79db35aa0c2b3ceeccb64292eb481bd3d018604
Harry Croker <hj.croker@gmail.com>
2025-09-01 22:31:06 +0100

    Chore: Improved overall code quality

--- DartsGame/Assets/Scripts/Dart.cs
+++ DartsGame/Assets/Scripts/Dart.cs
@@ -7,22 +7,43 @@ public class Dart : MonoBehaviour
 {
     private Rigidbody rb;
     private bool onBoard;
+    
+    // Called when the dart hits the dartboard
+    public delegate void HitBoardEventHandler();
+    public static HitBoardEventHandler HitBoard;
+    
+    // Dart Stats
+    [SerializeField, Range(0f, 100f), Tooltip("Multiplied by input vector magnitude to determine force")]
+    private float ForceMagnifier;
+    [SerializeField, Range(0f, 100f), Tooltip("Z Force applied to dart when launched")]
+    private float ForwardForce;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
-    
-    public delegate void HitBoardEventHandler();
-    public static HitBoardEventHandler HitBoard;
 
+    ///<summary>
+    /// Apply force to the dart based on the player's input
+    /// <param name="direction">The normalised direction vector of the player's swipe </param>
+    /// <param name="force">The magnitude of the player's swipe </param>
+    /// <param name="time">The time the player was touching the screen while swiping</param>
+    /// </summary>
     public void Launch(Vector2 direction, float force, float time)
     {
+        // Don't launch Dart if already in air
+        if(rb.isKinematic == false) return;
+
+        force *= ForceMagnifier;
+        
         rb.isKinematic = false;
-        //force /= time;
-        Vector3 forceVector = new Vector3(direction.x, -direction.y, -5) * force;
+        Vector3 forceVector = new Vector3(direction.x * force, -direction.y * force, -ForwardForce);
         rb.AddForce(forceVector, ForceMode.Impulse);
     }
     
+    /// <summary>
+    /// Reset this dart to its starting location
+    /// </summary>
     public void Reset()
     {
         rb.isKinematic = true;
@@ -40,11 +61,4 @@ public class Dart : MonoBehaviour
         HitBoard?.Invoke();
     }
 
-    // private void OnCollisionEnter(Collision other)
-    // {
-    //     if(!other.gameObject.CompareTag("Dartboard")) return;
-    //     rb.isKinematic = true;
-    //     print("Collision");
-    //     HitBoard?.Invoke();
-    // }
 }
--- DartsGame/Assets/Scripts/SwipeInput.cs
+++ DartsGame/Assets/Scripts/SwipeInput.cs
@@ -3,33 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class SwipeInput : MonoBehaviour
 {
+    // Input / Swipe
     private TouchControls touchControls;
     private Vector2 swipeStart, swipeEnd;
     private float startTime, endTime;
-    public Dart currentDart;
+    
+    // Darts
+    public Dart CurrentDart;
     private Vector3 defaultDartPosition;
     private Quaternion defaultDartRotation;
+    
     public GameObject DartPrefab;
 
     private void Awake()
     {
+        // Initialise player's input
         touchControls = new TouchControls();
         touchControls.Enable();
+        
         touchControls.Touch.Press.started += TouchStarted;
         touchControls.Touch.Press.canceled += TouchEnded;
-        //touchControls.Touch.Reset.performed += Reset;
     }
-
-    private void Start()
-    {
-        defaultDartPosition = currentDart.transform.position;
-        defaultDartRotation = currentDart.transform.rotation;
-        Dart.HitBoard += CreateNewDart;
-    }
-
+    
     private void OnEnable()
     {
         touchControls.Enable();
@@ -40,41 +39,64 @@ public class SwipeInput : MonoBehaviour
         touchControls.Disable();
     }
 
+    private void Start()
+    {
+        defaultDartPosition = CurrentDart.transform.position;
+        defaultDartRotation = CurrentDart.transform.rotation;
+        Dart.HitBoard += CreateNewDart;
+    }
+
+    /// <summary>
+    /// Called when player starts touching the screen to begin a swipe
+    /// </summary>
     private void TouchStarted(InputAction.CallbackContext context)
     {
         swipeStart = touchControls.Touch.Position.ReadValue<Vector2>();
         startTime = Time.time;
     }
     
+    /// <summary>
+    /// Called when player stops touching the screen, ending the swipe and launching a dart
+    /// </summary>
     private void TouchEnded(InputAction.CallbackContext context)
     {
+        // Calculate time length of swipe, return if swipe too long ot short
         endTime = Time.time;
         float timeHeld = endTime - startTime;
-        if(timeHeld < 0.1f) return;
+        if(timeHeld < 0.05f || timeHeld > 1) return;
         
         swipeEnd = touchControls.Touch.Position.ReadValue<Vector2>();
+        
         Vector2 swipeVector = swipeStart - swipeEnd;
         float swipeMagnitude = CorrectForScreenSize(swipeVector).magnitude;
         Vector2 swipeDirection = swipeVector.normalized;
-        print(swipeStart + " + " + swipeEnd + " = " + swipeVector);
-        print(swipeDirection + "     " + swipeMagnitude);
         
-        currentDart.Launch(swipeDirection, swipeMagnitude * 50, timeHeld);
+        CurrentDart.Launch(swipeDirection, swipeMagnitude, timeHeld);
     }
     
-    private Vector2 CorrectForScreenSize(Vector2 vector)
+    /// <summary>
+    /// Convert Vector from length in pixels to % of screen
+    /// <param name="vector">Input Vector in pixels </param>
+    /// <returns>Converted Vector in % of screen</returns>
+    /// </summary>
+    private static Vector2 CorrectForScreenSize(Vector2 vector)
     {
         return new Vector2(vector.x / Screen.width, vector.y / Screen.height);
     }
 
+    /// <summary>
+    /// Reset current dart to starting location
+    /// </summary>
     public void Reset()
     {
-        currentDart.Reset();
+        CurrentDart.Reset();
     }
 
+    /// <summary>
+    /// </summary>
     private void CreateNewDart()
     {
-        currentDart = Instantiate(DartPrefab, defaultDartPosition, 
+        CurrentDart = Instantiate(DartPrefab, defaultDartPosition, 
             defaultDartRotation).GetComponent<Dart>();
     }
 }
