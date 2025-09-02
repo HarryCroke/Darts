4121c32bd9122c7b1f2d3bc65281bc4db2fded90
Harry Croker <hj.croker@gmail.com>
2025-09-02 21:37:06 +0100

    Chore: Improved code quality of scoring code

--- DartsGame/Assets/Scripts/Dart.cs
+++ DartsGame/Assets/Scripts/Dart.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Dart : MonoBehaviour
 {
     private Rigidbody rb;
+    // Is the dart stuck to the dartboard?
     private bool onBoard;
     
     // Called when the dart hits the dartboard
@@ -19,9 +20,10 @@ public class Dart : MonoBehaviour
     [SerializeField, Range(0f, 100f), Tooltip("Z Force applied to dart when launched")]
     private float ForwardForce;
     
-    int baseScore, totalScore, multiplier;
+    private int baseScore, totalScore, multiplier;
     
-    // Distance / Multiplier
+    // Score multiplier is calculated by the dart's distance from the bullseye
+    // Key - Maximum distance from Bullseye, Value - respective multiplier
     private static Dictionary<float, int> multiplierDistances = new Dictionary<float, int>
     {
         {0.3f, 2}, // Double Bull
@@ -30,16 +32,16 @@ public class Dart : MonoBehaviour
         {2.85f, 3}, // Triples
         {4.15f, 1}, // Singles 2
         {4.45f,2} // Doubles
-        //{1000f, 0} // Out
     };
 
+    // Base score is calculated using the angle between the bullseye
+    // and the dart to calculate which segment the dart is in
+    // This array stores the score of each 18 degree segment
     private static int[] scoreArray = new int[]
     {
         11, 14, 9, 12, 5, 20, 1, 18, 4, 13, 6, 10, 15, 2, 17, 3, 19, 7, 16, 8
 
     };
-
-    public GameObject be;
     
     private void Awake()
     {
@@ -83,26 +85,29 @@ public class Dart : MonoBehaviour
         HitBoard?.Invoke();
         Dartboard dartboard = other.transform.parent.GetComponent<Dartboard>();
 
-        CalculateScore(dartboard.Bullseye);
+        totalScore = CalculateScore(dartboard.Bullseye);
         string scoreText = (baseScore + " x " + multiplier + " = " + totalScore);
         GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = scoreText;
-
-        // float scoreMultiplier = GetScoreFromDistance(GetDistanceFromBullseye(dartboard.Bullseye));
-        // GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = scoreMultiplier.ToString();
-        // be = other.gameObject;
-
     }
 
-    private float CalculateScore(GameObject bullseye)
+    /// <summary>
+    /// Calculate and return the score of this dart when hitting the board
+    /// <param name="bullseye">Reference to a game-object at the centre of the dartboard</param>
+    /// </summary>
+    private int CalculateScore(GameObject bullseye)
     {
-        // Score may be overwritten by bullseye
         baseScore = GetScoreFromAngle(GetAngleFromBullseye(bullseye));
+        
+        // Base score will be overwritten if a bullseye is hit
         multiplier = GetMultiplierFromDistance(GetDistanceFromBullseye(bullseye));
         
-        totalScore = baseScore * multiplier;
-        return totalScore;
+        return baseScore * multiplier;
     }
 
+    /// <summary>
+    /// Return the 2D distance between this object and the Bullseye. Z position is ignored
+    /// <param name="bullseye">Reference to a game-object at the centre of the dartboard</param>
+    /// </summary>
     private float GetDistanceFromBullseye(GameObject bullseye)
     {
         Vector2 dartLocation = new Vector2(transform.position.x, transform.position.y);
@@ -110,21 +115,21 @@ public class Dart : MonoBehaviour
         return Vector2.Distance(dartLocation, bullseyeLocation);
     }
 
+    /// <summary>
+    /// The score multiplier (e.g double/triple) is determined by the distance the dart landed from the bullseye.
+    /// This method also overrides 'baseScore' if this dart hit a bullseye
+    /// <param name="distance">Distance between this dart and the bullseye</param>
+    /// </summary>
     private int GetMultiplierFromDistance(float distance)
     {
         foreach (var item in multiplierDistances)
         {
             if (distance < item.Key)
             {
-                // Bullseye
-                switch (item.Key)
+                // Override score if bullseye hit
+                if (distance < 0.55)
                 {
-                    case 0.3f:
-                        baseScore = 25;
-                        break;
-                    case 0.55f:
-                        baseScore = 25;
-                        break;
+                    baseScore = 25;
                 }
                 
                 return item.Value;
@@ -134,16 +139,29 @@ public class Dart : MonoBehaviour
         return 0;
     }
 
+    /// <summary>
+    /// Return the angle of the dart from the bullseye from 0-360, rotated so that 0 is the start of a segment.
+    /// Z position is ignored.
+    /// <param name="bullseye">Reference to a game-object at the centre of the dartboard</param>
+    /// </summary>
     private float GetAngleFromBullseye(GameObject bullseye)
     {
         Vector2 dartLocation = new Vector2(transform.position.x, transform.position.y);
         Vector2 bullseyeLocation = new Vector2(bullseye.transform.position.x, bullseye.transform.position.y);
         Vector2 diff = dartLocation - bullseyeLocation;
+        
+        // 9 is added as it is half of the angle of each segment, otherwise 0 would fall in the centre of a segment
         float angle =  (Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg) + 9;
+        
+        // Convert minus degrees to positive degrees
         if (angle < 0) angle += 360;
         return angle;
     }
 
+    /// <summary>
+    /// Return the base score of the segment the dart landed in. 
+    /// <param name="angle">The angle of the dart from the bullseye between 0-360</param>
+    /// </summary>
     private int GetScoreFromAngle(float angle)
     {
         for (int i = 0; i < scoreArray.Length; i++)
@@ -153,13 +171,7 @@ public class Dart : MonoBehaviour
                 return scoreArray[i];
             }
         }
-
         return 0;
     }
 
-    private void Update()
-    {
-        if(!be) return;
-        print(GetAngleFromBullseye(be));
-    }
 }
